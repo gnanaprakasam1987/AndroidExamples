4654bf8c32446ea06006972cf99beff6faf62167
gnanaprakasam.d <Android@123>
2019-07-22 12:52:57 +0530

    BO Class Committed

---
+++ app/src/main/java/com/example/RetrofitWithMVP/Presenter/MovieDetailBO.java
@@ -0,0 +1,56 @@
+package com.example.RetrofitWithMVP.Presenter;
+
+import com.google.gson.annotations.SerializedName;
+
+public class MovieDetailBO {
+
+   /* "albumId": 1,
+            "id": 1,
+            "title": "accusamus beatae ad facilis cum similique qui sunt",
+            "url": "https://via.placeholder.com/600/92c952",
+            "thumbnailUrl": "https://via.placeholder.com/150/92c952"*/
+
+    @SerializedName("albunid")
+    private int id;
+
+    @SerializedName("title")
+    private String title;
+
+    @SerializedName("url")
+    private String url;
+
+    @SerializedName("thumbnailUrl")
+    private String thumbnailUrl;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getThumbnailUrl() {
+        return thumbnailUrl;
+    }
+
+    public void setThumbnailUrl(String thumbnailUrl) {
+        this.thumbnailUrl = thumbnailUrl;
+    }
+}
